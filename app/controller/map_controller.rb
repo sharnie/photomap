7d2833d70c6a99760c3eb66b6b98d4f0c6c50caa
Sharnie Ivery <sharnieivery@gmail.com>
2014-07-04 09:22:09 -0400

    Implement image template using underscore

--- app/controller/map_controller.rb
+++ app/controller/map_controller.rb
@@ -2,4 +2,93 @@ class MapController < ApplicationController
   get '/' do
     erb :'map/index'
   end
+
+  # login url
+  get "/oauth/connect" do
+    redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL, :scope => 'relationships likes')
+  end
+
+  # logout url
+  get "/oauth/logout" do
+    session.clear
+    redirect '/'
+  end
+
+  # redirect user after authentication on Instagram and set access token
+  get "/oauth/callback" do
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    session[:access_token] = response.access_token
+
+    client                 = Instagram.client(access_token: session[:access_token])
+    session[:instagram_id] = client.user.id
+
+    User.find_or_create_by(instagram_id: client.user.id) do |user|
+             user.username = client.user.username
+    end
+
+    redirect '/'
+  end
+
+  get '/map/:lat/:lng/media_search.json' do
+    content_type :json
+    locations = []
+    media = user_signed_in? ? CLIENT : Instagram
+
+    media.media_search(params[:lat], params[:lng], distance: 10).each do |data|
+      caption = data.caption ? data.caption.text : ''
+      locations <<  {
+                      latitude:             data.location.latitude,
+                      longitude:            data.location.longitude,
+                      location_name:        data.location.name,
+                      low_resolution:       data.images.low_resolution.url,
+                      standard_resolution:  data.images.standard_resolution.url,
+                      caption_text:         caption,
+                      id:                   data.id,
+                      likes:                data.likes,
+                      user_has_liked:       data.user_has_liked,
+                      created_time:         data.created_time,
+                      user: {   
+                        id:                 data.user.id,
+                        username:           data.user.username,
+                        profile_picture:    data.user.profile_picture,
+                        bio:                data.user.bio,
+                        full_name:          data.user.full_name,
+                      }
+                    }
+
+    end
+
+    locations.to_json
+  end
+
+  get '/media/:media_id.json' do
+    content_type :json
+    response = CLIENT.media_item(params[:media_id])
+    response.to_json
+  end
+
+  get '/like/:image_id' do
+    content_type :json
+    response = CLIENT.like_media(params[:image_id])
+    response.to_json
+  end
+
+  # GET, MODIFY RELATIONSHIP
+  get '/users/:id/relationship.json' do
+    content_type :json
+    response = CLIENT.user_relationship(params[:id])
+    response.merge({id: params[:id]}).to_json
+  end
+
+  get '/users/:id/follow.json' do
+    content_type :json
+    response = CLIENT.follow_user(params[:id])
+    response.to_json
+  end
+
+  get '/users/:id/unfollow.json' do
+    content_type :json
+    response = CLIENT.unfollow_user(params[:id])
+    response.to_json
+  end
 end
--- public/js/map.js
+++ public/js/map.js
@@ -4,29 +4,99 @@ $(document).ready(function(){
     var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
 
     var mapOptions = {
-      center: myLatlng,
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      streetViewControl: false,
-      mapTypeControl: false,
-      panControl: false,
+        center            : myLatlng,
+        zoom              : 12,
+        mapTypeId         : google.maps.MapTypeId.ROADMAP,
+        streetViewControl : false,
+        mapTypeControl    : false,
+        panControl        : false,
     };
 
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     var marker_image = {
-      url: '/images/marker3.png',
-      origin: new google.maps.Point(0,0),
-      anchor: new google.maps.Point(0,0)
+        url         : '/images/marker3.png',
+        origin      : new google.maps.Point(0,0),
+        anchor      : new google.maps.Point(0,0)
     };
 
     var marker = new google.maps.Marker({
         position    : myLatlng,
         map         : map,
-        title       : 'Hello World!',
         icon        : marker_image,
     });
 
     google.maps.event.addDomListener(window, 'load', initialize);
+
+    // Create the search box and link it to the UI element.
+    var input = (document.getElementById('place-search'));
+
+    var searchBox = new google.maps.places.SearchBox((input));
+
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+        if (places.length == 0) {
+          return;
+        }
+
+        // fetchImage(places[0].geometry.location.k, places[0].geometry.location.A);
+        map.panTo(places[0].geometry.location);
+        map.setZoom(15);
+    });
+
   })();
+
+  // get images
+  function getImages(lat, lng){
+    var url = '/map/' + lat + '/' + lng + '/media_search.json';
+
+
+    $.getJSON(url).success(function(response){
+      var html = [];
+      var templateString = [
+      "        <div class='col-md-4'>",
+      "          <div class='thumbnail'>",
+      "            <div class='media-head'>",
+      "              <img src='<%= image.low_resolution %>' alt='...'>",
+      "              <span class='caption'><%= image.caption_text %></span>",
+      "            </div>",
+      "            <div class='media-foot'>",
+      "              <div class='col-xs-8'>",
+      "                <div class='row'>",
+      "                  <div class='media'>",
+      "                    <a class='avatar pull-left' href='#'>",
+      "                      <img class='thumb-sm' src='<%= image.user.profile_picture %>' alt='...'>",
+      "                    </a>",
+      "                    <div class='media-user text-muted'>",
+      "                      <a class='bold username text-ellipsis' href='#'><%= image.user.username %></a>",
+      "                      <p class='text-ellipsis'><%= image.user.full_name %></p>",
+      "                    </div>",
+      "                  </div><!-- .media -->",
+      "                </div><!-- .row -->",
+      "              </div><!-- .col-xs-* -->",
+      "              <div class='col-xs-4'>",
+      "                <div class='row'>",
+      "                  <div class='pull-right'>",
+      "                  <button class='btn btn-primary'>",
+      "                    <i class='fa fa-plus'></i>",
+      "                    <i class='fa fa-user'></i>",
+      "                  </button>",
+      "                  </div>",
+      "                </div>",
+      "              </div>",
+      "            </div><!-- .caption -->",
+      "          </div><!-- .thumbnail -->",
+      "        </div><!-- col-md-6-* -->"]
+      _.each(response, function(image){
+        // var templateString = ('<img src="<%= low_resolution %>"/>')
+        var template = _.template(templateString.join("\n"));
+        html += template({image: image});
+      });
+      $('#result').prepend(html);
+      // console.log(html.join());
+    });
+  }
+  getImages(37.7808851, -122.3948632);
+
+
 });
